3f83a2cbbece71e609798b17e7bfc84b5563cd64
Pavel <skydish101@gmail.com>
2024-04-12 16:57:58 +0900

    First commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,20 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Layout from '../../components/layout';
+import styles from '../../styles/style1.module.css';
 
 export default function FirstPost() {
   return (
-    <Layout>
+    <>
       <Head>
         <title>My message</title>
       </Head>
-      <img class = "images" src="/images/photo1.jpg" alt="Pasha"  />
+      <img src="/images/photo1.jpg" alt="Pasha" className ={styles.myimg}  />
 
-      <h2 align = "middle">Hello, my name is Kim Pavel. I am a freshman student studying at Hanyang University and majoring in Information Systems. I have just edited this page for CS course assignment :3</h2>
-      <h2>
-        <Link href="/">Back to home</Link>
+      <h2 className ={styles.txt} >Hello, my name is Kim Pavel. I am a freshman student studying at Hanyang University and majoring in Information Systems. I have just edited this page for CS course assignment :3      </h2>
+      <h2 className ={styles.txt} ><Link className ={styles.myimg} href="https://www.instagram.com/xskydish/">My instagram</Link>   </h2>
+      <h2 className ={styles.txt}>
+        <Link className ={styles.myimg}  href="/">Back to home</Link>
       </h2>
-    </Layout>
+    </>
   );
 }
