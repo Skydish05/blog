d22e5b87b8d7dd74fd1ed321de0ec3777c8a8df7
Skydish05 <skydish101@gmail.com>
2024-04-15 00:09:59 +0900

    First commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,12 +8,13 @@ export default function FirstPost() {
       <Head>
         <title>My message</title>
       </Head>
-      <img src="/images/photo1.jpg" alt="Pasha" className ={styles.myimg}  />
+      <img src="/images/photo1.jpg" alt="Pasha" className ={styles.imgg}  />
 
-      <h2 className ={styles.txt} >Hello, my name is Kim Pavel. I am a freshman student studying at Hanyang University and majoring in Information Systems. I have just edited this page for CS course assignment :3      </h2>
+      <h2 className ={styles.txt} >Hello, my name is Kim Pavel. I am a freshman student studying at Hanyang University and majoring in Information Systems.
+       I have just edited this page for CS course assignment :3      </h2>
       <h2 className ={styles.txt} ><Link className ={styles.myimg} href="https://www.instagram.com/xskydish/">My instagram</Link>   </h2>
       <h2 className ={styles.txt}>
-        <Link className ={styles.myimg}  href="/">Back to home</Link>
+        <Link className ={styles.txt}  href="/">Back to home</Link>
       </h2>
     </>
   );
